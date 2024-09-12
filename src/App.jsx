591f9d03efcfc57db96a0c87296ed96d0c2f999d
BasmaJamaldine <you@example.com>
2024-09-12 11:36:11 +0100

    ajouter des dossiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/components/Home"
+import { About } from "./pages/about/components/About"
 import "./index.css"
 function App() {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </>
   );
 }
 
