caa38a65b5e4f8b2fa0a9fee3008203ba747a9b3
BasmaJamaldine <you@example.com>
2024-09-13 15:57:44 +0100

    ajouter blog dans home

--- src/pages/home/components/Home.jsx
+++ src/pages/home/components/Home.jsx
@@ -6,6 +6,7 @@ import { MdOutlineNavigateNext } from 'react-icons/md';
 import data from "../../../json/data.json"
 import Slider from "react-slick";
 import { images } from '../../../constants';
+import blogData from "../../../json/blogData.json"
 
  
 export const Home = () => {
@@ -184,6 +185,27 @@ export const Home = () => {
         <MdOutlineNavigateNext />
       </button>
     </div>
+    <div>
+    <h2 className="text-2xl md:text-3xl font-bold m-10 text-center">
+     OUR BLOG
+      </h2>
+  <div class="mx-8 my-10 gap-7 grid max-w-screen-xl gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
+  {blogData.map((item) => (
+  <div class="group cursor  w-[28vw] h-[70vh]  ">
+    <div className='overflow-hidden' >
+      <img src={images[item.pic]} className='transform transition-transform duration-300 hover:scale-110 w-full h-[40vh] mb-3 overflow-hidden' />
+    </div>
+    <div class="flex-1 overflow-hidden bg-white ">
+      <h1 className=' font-semibold hover:text-[#e65540] mb-3'>{item.title}</h1>
+      <p class=" text-gray-600 mb-3">{item.by}</p>
+      <p class=" text-gray-600">{item.description}</p>
+
+      </div>
+    </div>
+  ))}
+
+  </div>
+  </div>
     <div>
 <div className="w-full py-16 bg-white text-center">
 
